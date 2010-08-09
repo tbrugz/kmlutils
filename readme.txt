f41f85f5a293f4aa471a831971707bcc3d712b19
tbrugz <none@none>
2010-08-09 17:54:58 -0300

    added build.xml, readme, svg2kml.properties

---
+++ readme.txt
@@ -0,0 +1,28 @@
+
+kmlutils - utils for KML files
+------------------------------
+
+The main purpose of this project is a tool for converting SVG files into KML files. Currently it (svg2kml)
+partially converts polygons.
+
+svg2kml howto:
+- build with ant
+- edit svg2kml.properties, set "svgin", "kmlout", "maxX", "minX", "maxY", "minY" properties
+- (optional) edit snippets.properties to set constant parts to be inserted in the KML file
+- (optional) edit idmappings.properties to translate IDs from the SVG file to the KML file
+- run "java -cp dist/svg2kml.jar;lib/commons-logging-1.1.1.jar;lib/log4j-1.2.15.jar tbrugz/geo/SVG2KML"
+
+Author:
+Telmo Brugnara <tbrugz@gmail.com>
+
+License:
+GNU Lesser General Public License - http://www.gnu.org/licenses/lgpl.html
+
+Dependencies:
+- log4j (included)
+- commons-logging (included)
+- ant (not included - build only)
+
+Municipalities_of_RS.svg originated from:
+http://commons.wikimedia.org/wiki/File:Municipalities_of_the_South_Region_of_Brazil.svg
+licensed under: http://creativecommons.org/licenses/by-sa/3.0/deed.en
--- src/tbrugz/geo/SVG2KML.java
+++ src/tbrugz/geo/SVG2KML.java
@@ -1,7 +1,9 @@
 package tbrugz.geo;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.PrintStream;
+import java.util.Properties;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -13,23 +15,38 @@ public class SVG2KML {
 	static Log log = LogFactory.getLog(SVG2KML.class);
 
 	public static void main(String[] args) throws IOException {
+		Properties prop = new Properties();
+		prop.load(new FileInputStream("svg2kml.properties"));
+		String fileIn = prop.getProperty("svg2kml.svgin");
+		String fileOut = prop.getProperty("svg2kml.kmlout");
+		float outMaxX = Float.parseFloat(prop.getProperty("svg2kml.maxX"));
+		float outMinX = Float.parseFloat(prop.getProperty("svg2kml.minX"));
+		float outMaxY = Float.parseFloat(prop.getProperty("svg2kml.maxY"));
+		float outMinY = Float.parseFloat(prop.getProperty("svg2kml.minY"));
+		
+		log.info("parsing svg: "+fileIn);
+
 		SVGParser parser = new SVGParser();
-		Root root = parser.parseDocument("work/input/Municipalities_of_RS.svg");
+		//Root root = parser.parseDocument("work/input/Municipalities_of_RS.svg");
+		Root root = parser.parseDocument(fileIn);
 
 		log.info("parsed...");
 		
 		CoordinatesTransformer ct = new CoordinatesTransformer();
 		float[] inputBounds = {root.maxX, root.minX, root.maxY, root.minY};
-		float[] outputBounds = {-49.6917f, -57.64777f, -33.7515f, -27.08f,};
+		//float[] outputBounds = {-49.6917f, -57.64777f, -33.7515f, -27.08f,};
+		float[] outputBounds = {outMaxX, outMinX, outMaxY, outMinY,};
 		ct.transformCoords(root, inputBounds, outputBounds);
 
 		log.info("coords transformed...");
+		log.info("writing kml: "+fileOut);
 		
 		DumpKMLModel dm = new DumpKMLModel();
 		//dm.dumpModel(parser.root, System.out);
-		dm.dumpModel(root, new PrintStream("work/output/Municipalities_of_RS_out.kml"));
+		//dm.dumpModel(root, new PrintStream("work/output/Municipalities_of_RS_out.kml"));
+		dm.dumpModel(root, new PrintStream(fileOut));
 
-		log.info("dump done...");
+		log.info("write done...");
 	}
 
 }
